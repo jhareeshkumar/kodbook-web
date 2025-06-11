24ff423c69206ce11e0b424f5b500d2dc487bd5a
jhareeshkumar <jhareesh30@gmail.com>
2025-06-12 01:39:25 +0530

    basic project structre ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { AuthLayout } from './layout/auth-layout/auth-layout';
+import { MainLayout } from './layout/main-layout/main-layout';
+import { Feed } from './features/feed/feed';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'feed',
+        pathMatch: 'full'
+    },
+    {
+        path: 'auth',
+        component: AuthLayout,
+        children: [
+            { path: 'login', component: Login },
+            { path: 'register', component: Register }
+        ]
+    },
+    {
+        path: '',
+        component: MainLayout,
+        children: [
+            { path: 'feed', component: Feed }
+        ]
+    },
+];
